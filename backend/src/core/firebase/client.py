9486ba3ff615e9ceb3fccbf7c67ec75c9c5ce3d8
mrdsx <andre0.castlegames@gmail.com>
2026-04-07 17:19:06 +0300

    refac: add loading dotenv variables

--- backend/src/core/firebase/client.py
+++ backend/src/core/firebase/client.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
 from firebase_admin import initialize_app  # pyright: ignore[reportUnknownVariableType]
 from firebase_admin.credentials import Certificate
 
 from .config import service_account
+
+# ! NEVER DELETE THIS LINE
+# otherwise app can't inject firebase emulators hosts
+# and firebase will connect to production databaseE
+load_dotenv()
 
 
 def initialize_firebase() -> None:
