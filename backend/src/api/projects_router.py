170ddcd6dd8aa56e215da83094a6ee2c3a484946
mrdsx <andre0.castlegames@gmail.com>
2026-04-11 15:01:37 +0300

    chore: uncomment redis code

--- backend/src/api/projects_router.py
+++ backend/src/api/projects_router.py
@@ -1,12 +1,17 @@
+import json
 from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends
+from pydantic import ValidationError
+from redis.asyncio import Redis
 
 from src.api.dependencies import (
     get_firestore,
     get_projects_reports_repository,
     get_projects_reports_service,
+    get_redis,
 )
+from src.core.constants import CACHE_TTL_SECONDS, RedisKeys
 from src.core.firebase.types import AsyncFirestore
 from src.repositories.projects_reports import ProjectsReportsRepository
 from src.schemas.projects_reports import ProjectsReportsOut
@@ -18,6 +23,7 @@ router = APIRouter(prefix="/projects")
 @router.get("/")
 async def get_projects_reports(
     db: Annotated[AsyncFirestore, Depends(get_firestore)],
+    redis: Annotated[Redis, Depends(get_redis)],
     reports_service: Annotated[
         ProjectsReportsService, Depends(get_projects_reports_service)
     ],
@@ -26,24 +32,22 @@ async def get_projects_reports(
         Depends(get_projects_reports_repository),
     ],
 ) -> ProjectsReportsOut:
-    # TODO: uncomment after testing in production
-    # try:
-    #     cached_reports = await redis.get(name=RedisKeys.PROJECTS_REPORTS)
-    #     if cached_reports is not None:
-    #         return ProjectsReportsOut(projects=json.loads(cached_reports))
-    # except ValidationError, json.JSONDecodeError:
-    #     pass
+    try:
+        cached_reports = await redis.get(RedisKeys.PROJECTS_REPORTS)
+        if cached_reports is not None:
+            return ProjectsReportsOut.model_validate_json(cached_reports)
+    except ValidationError, json.JSONDecodeError:
+        pass
 
     projects_reports = await reports_service.get_projects_reports(
         reports_repository=reports_repository, db=db
     )
 
-    # TODO: uncomment after testing in production
-    # await redis.set(
-    #     name=RedisKeys.PROJECTS_REPORTS,
-    #     value=projects_reports.model_dump_json(),
-    #     ex=CACHE_TTL_SECONDS,
-    # )
+    await redis.set(
+        RedisKeys.PROJECTS_REPORTS,
+        projects_reports.model_dump_json(),
+        ex=CACHE_TTL_SECONDS,
+    )
 
     return projects_reports
 
