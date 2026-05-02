2d17ffceeda1b59070651bb3b6c84353e39eb804
mrdsx <andre0.castlegames@gmail.com>
2026-05-02 20:19:50 +0300

    chore: update 'start.prod.sh' script

--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker compose -f compose.prod.yaml up -d & disown
-cd webhooks/github && uv run uvicorn src.main:app --host 0.0.0.0 --port 8010 --reload & disown
+cd webhooks/github && nohup uv run uvicorn src.main:app --host 0.0.0.0 --port 8010 --reload > /dev/null 2>&1 & disown
