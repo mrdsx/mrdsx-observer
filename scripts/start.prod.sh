9c1912e16e62e3f570b2420bb073cc60c337435f
mrdsx <andre0.castlegames@gmail.com>
2026-05-02 21:06:40 +0300

    fix: fix 'start.prod.sh' script

--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd $HOME/mrdsx-observer
 docker compose -f compose.prod.yaml up -d & disown
-cd webhooks/github && nohup uv run uvicorn src.main:app --host 0.0.0.0 --port 8010 --reload > /dev/null 2>&1 & disown
+
+cd $HOME/mrdsx-observer/webhooks/github
+/usr/bin/nohup $HOME/.local/bin/uv run uvicorn src.main:app --host 0.0.0.0 --port 8010 > /dev/null 2>&1 &
