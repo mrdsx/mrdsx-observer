a4d3eb7b506d253d5366cfc9d5ee325fbe03e0ac
mrdsx <andre0.castlegames@gmail.com>
2026-05-23 20:51:11 +0300

    test: update specs
    added: 'test_insert_report'
    updated: 'test_update_report', 'test_delete_old_reports'

--- backend/tests/repositories/test_projects_reports_repositories.py
+++ backend/tests/repositories/test_projects_reports_repositories.py
@@ -70,26 +70,60 @@ def raw_daily_reports() -> list[dict[str, Any]]:
     ]
 
 
+
+@pytest.mark.asyncio
+async def test_insert_report(session: AsyncSession):
+    projects_reports_repository = get_projects_reports_repository()
+
+    await projects_reports_repository.insert_report(
+        project_id="project1",
+        current_date=datetime(year=2027, month=1, day=1),
+        services_reports={
+            "service": ProjectServiceReport(
+                current_status="operational",
+                operational=1,
+                degraded=0,
+                outages=0,
+            )
+        },
+        session=session,
+    )
+    result = await session.execute(select(DB_DailyProjectReport))
+    raw_reports = deserialize_rows(result)
+    assert len(raw_reports) == 1
+    assert raw_reports[0] == {
+        "project_id": "project1",
+        "date_str": "2027-01-01",
+        "created_at": datetime(year=2027, month=1, day=1),
+        "services_reports": {
+            "service": {
+                "current_status": "operational",
+                "operational": 1,
+                "degraded": 0,
+                "outages": 0,
+            }
+        },
+    }
+
+
 @pytest.mark.asyncio
 async def test_update_report(
     session: AsyncSession, raw_daily_reports: list[dict[str, Any]]
 ):
     projects_reports_repository = get_projects_reports_repository()
-    statement = select(DB_DailyProjectReport).where(
-        DB_DailyProjectReport.project_id == "project2",
-        DB_DailyProjectReport.date_str == "2027-01-02",
-    )
 
     session.add_all([DB_DailyProjectReport(**report) for report in raw_daily_reports])
-    result = await session.execute(statement)
-    assert deserialize_rows(result)[0] == {
+    result = await session.execute(select(DB_DailyProjectReport))
+    raw_reports = deserialize_rows(result)
+    assert len(raw_reports) == len(raw_daily_reports)
+    assert raw_reports[-1] == {
         "project_id": "project2",
         "date_str": "2027-01-02",
         "created_at": datetime(year=2027, month=1, day=2),
         "services_reports": {
             "service": {
                 "current_status": "degraded",
-                "operational": 0,  # this field WILL be updated
+                "operational": 0,  # this field WILL BE updated
                 "degraded": 1,
                 "outages": 0,
             }
@@ -109,15 +143,17 @@ async def test_update_report(
         },
         session=session,
     )
-    result = await session.execute(statement)
-    assert deserialize_rows(result)[0] == {
+    result = await session.execute(select(DB_DailyProjectReport))
+    raw_reports = deserialize_rows(result)
+    assert len(raw_reports) == len(raw_daily_reports)
+    assert raw_reports[-1] == {
         "project_id": "project2",
         "date_str": "2027-01-02",
         "created_at": datetime(year=2027, month=1, day=2),
         "services_reports": {
             "service": {
                 "current_status": "operational",
-                "operational": 2,  # this field SHOULD be updated
+                "operational": 2,  # this field IS updated
                 "degraded": 1,
                 "outages": 0,
             }
@@ -133,10 +169,38 @@ async def test_delete_old_reports(
 
     session.add_all([DB_DailyProjectReport(**report) for report in raw_daily_reports])
     result = await session.execute(select(DB_DailyProjectReport))
-    assert len(result.scalars().all()) == 4
+    assert len(result.scalars().all()) == len(raw_daily_reports)
 
     await projects_reports_repository.delete_old_reports(
         cutoff_date=datetime(year=2027, month=1, day=2), session=session
     )
     result = await session.execute(select(DB_DailyProjectReport))
-    assert len(result.scalars().all()) == 2
+    raw_reports = deserialize_rows(result)
+    assert raw_reports == [
+        {
+            "project_id": "project1",
+            "date_str": "2027-01-02",
+            "created_at": datetime(year=2027, month=1, day=2),
+            "services_reports": {
+                "service": {
+                    "current_status": "outage",
+                    "operational": 0,
+                    "degraded": 0,
+                    "outages": 1,
+                }
+            },
+        },
+        {
+            "project_id": "project2",
+            "date_str": "2027-01-02",
+            "created_at": datetime(year=2027, month=1, day=2),
+            "services_reports": {
+                "service": {
+                    "current_status": "degraded",
+                    "operational": 0,
+                    "degraded": 1,
+                    "outages": 0,
+                }
+            },
+        },
+    ]
