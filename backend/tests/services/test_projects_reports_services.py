e38da6efec851099e3300a9c7df52f6c386db767
mrdsx <andre0.castlegames@gmail.com>
2026-05-28 11:03:37 +0300

    test: add specs for class 'ProjectsReportsService'

--- backend/tests/services/test_projects_reports_services.py
+++ backend/tests/services/test_projects_reports_services.py
@@ -1,6 +1,8 @@
 import pytest
 from httpx import AsyncClient
 
+from src.api.dependencies import get_projects_reports_service
+from src.core.models.projects_reports import DailyProjectReportDTO, ProjectReportDTO
 from src.services.projects_reports import ProjectsStateSnapshotter
 
 
@@ -32,3 +34,97 @@ class TestProjectsStateSnapshotter:
         assert status == {
             "Site": "operational",
         }
+
+
+class TestProjectsReportsService:
+    projects_reports_service = get_projects_reports_service()
+
+    def test_map_projects_reports(self):
+        projects_reports = {
+            "project1": ProjectReportDTO(
+                name="Project 1",
+                daily_reports={
+                    "2027-01-01": DailyProjectReportDTO(
+                        worst_status="operational",
+                        uptime=100,
+                    )
+                },
+                current_status="operational",
+                uptime=100,
+            ),
+            "project2": ProjectReportDTO(
+                name="Project 2",
+                daily_reports={
+                    "2027-01-01": DailyProjectReportDTO(
+                        worst_status="degraded",
+                        uptime=100,
+                    )
+                },
+                current_status="degraded",
+                uptime=100,
+            ),
+            "project3": ProjectReportDTO(
+                name="Project 3",
+                daily_reports={
+                    "2027-01-01": DailyProjectReportDTO(
+                        worst_status="operational",
+                        uptime=100,
+                    ),
+                    "2027-01-02": DailyProjectReportDTO(
+                        worst_status="outage",
+                        uptime=0,
+                    ),
+                },
+                current_status="outage",
+                uptime=50,
+            ),
+        }
+        mapped_projects_reports = self.projects_reports_service._map_projects_reports(
+            projects_reports=projects_reports
+        )
+        assert mapped_projects_reports == [
+            {
+                "id": "project1",
+                "name": "Project 1",
+                "daily_reports": [
+                    {
+                        "date": "2027-01-01",
+                        "worst_status": "operational",
+                        "uptime": 100,
+                    },
+                ],
+                "current_status": "operational",
+                "uptime": 100,
+            },
+            {
+                "id": "project2",
+                "name": "Project 2",
+                "daily_reports": [
+                    {
+                        "date": "2027-01-01",
+                        "worst_status": "degraded",
+                        "uptime": 100,
+                    },
+                ],
+                "current_status": "degraded",
+                "uptime": 100,
+            },
+            {
+                "id": "project3",
+                "name": "Project 3",
+                "daily_reports": [
+                    {
+                        "date": "2027-01-01",
+                        "worst_status": "operational",
+                        "uptime": 100,
+                    },
+                    {
+                        "date": "2027-01-02",
+                        "worst_status": "outage",
+                        "uptime": 0,
+                    },
+                ],
+                "current_status": "outage",
+                "uptime": 50,
+            },
+        ]
