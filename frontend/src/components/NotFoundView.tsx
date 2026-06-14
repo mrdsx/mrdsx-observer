eb0ca7d2fcbc4d5596c43d5c1c42ca4ac2cc1521
mrdsx <andre0.castlegames@gmail.com>
2026-06-14 15:52:10 +0300

    chore: update NotFoundView.tsx

--- frontend/src/components/NotFoundView.tsx
+++ frontend/src/components/NotFoundView.tsx
@@ -1,12 +1,10 @@
 import { Title } from "@solidjs/meta";
-import { HttpStatusCode } from "@solidjs/start";
 import { Button } from "@/components/ui/button";
 
 export function NotFoundView() {
   return (
     <div class="self-center py-20 flex flex-col items-center gap-8">
       <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
       <span class="flex flex-col gap-2 items-center">
         <p class="font-semibold text-xl">404</p>
         <h1 class="text-xl">Page Not Found</h1>
