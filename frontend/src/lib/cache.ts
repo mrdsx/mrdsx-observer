ca2ed5b55d1ee0a79fef3c06ea403fac0357b9ab
mrdsx <andre0.castlegames@gmail.com>
2026-06-16 11:42:58 +0300

    feat(frontend): add deferred cache revalidation

--- frontend/src/lib/cache.ts
+++ frontend/src/lib/cache.ts
@@ -3,20 +3,41 @@ type CachedProps<TData> = {
   fetchKey: string;
 };
 
-const cache: Record<string, unknown> = {};
+// Whatever number I put - it just looked good to me
+// (30 seconds but in milliseconds)
+const STALE_TIME = 30 * 1000;
+const cache: Record<string, { result: unknown; revalidateAt: Date }> = {};
 
+function getRevalidationTime(date: Date): Date {
+  return new Date(date.getTime() + STALE_TIME);
+}
+
+/*
+ * Stale-while-revalidate caching
+ */
 export async function cached<TData>({ fetchFn, fetchKey }: CachedProps<TData>) {
   const cachedData = cache[fetchKey];
   if (cachedData === undefined) {
     const result = await fetchFn();
-    cache[fetchKey] = result;
+    const now = new Date();
+    cache[fetchKey] = {
+      result,
+      revalidateAt: getRevalidationTime(now),
+    };
+
     return result;
   }
 
   setTimeout(async () => {
     const result = await fetchFn();
-    cache[fetchKey] = result;
+    const now = new Date();
+    if (cachedData.revalidateAt <= now) {
+      cache[fetchKey] = {
+        result,
+        revalidateAt: getRevalidationTime(now),
+      };
+    }
   });
 
-  return cachedData as TData;
+  return cachedData.result as TData;
 }
